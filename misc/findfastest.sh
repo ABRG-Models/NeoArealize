1191d4110fb75970d96698544ff2f34a1be4e741
Seb James <seb.james@sheffield.ac.uk>
2019-02-06 11:41:25 +0000

    Small change to test only up to the number of physical cores on the system

--- misc/findfastest.sh
+++ misc/findfastest.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# Find out how many threads will be fastest, up to the number of cores on the system
-NUMCORES=`nproc --all`
+# Find out how many threads will be fastest, up to the number of
+# (physical) cores on the system
+NUMCORES=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+echo "This system has ${NUMCORES} physical cores"
 CONFIGFILE='configs/c1.json'
 for i in $(seq 1 $NUMCORES); do
     echo -n "$i cores:"

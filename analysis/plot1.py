b3db6cc38802a8fed8d27b8fa4a61fd57adb1e84
Seb James <seb.james@sheffield.ac.uk>
2019-02-12 11:27:09 +0000

    Most recent configs and minor updates to python plotting

--- analysis/plot1.py
+++ analysis/plot1.py
@@ -17,15 +17,16 @@ matplotlib.rc('font', **fnt)
 
 # Get target x/y hex to show trace for and the time step to show the
 # map for from the arguments:
-if len(sys.argv) < 4:
-    print('Provide x,y, and t index on cmd line please.')
+if len(sys.argv) < 5:
+    print('Provide filepath, x, y, and t index on cmd line please.')
     exit(1)
-trgx = float(sys.argv[1])
-trgy = float(sys.argv[2])
-trgt = int(sys.argv[3])
+thefilename = sys.argv[1]
+trgx = float(sys.argv[2])
+trgy = float(sys.argv[3])
+trgt = int(sys.argv[4])
 
 # Read the data
-(x, y, t, cmatrix, amatrix, nmatrix) = ld.readFiles ('../logs/2N1M')
+(x, y, t, cmatrix, amatrix, nmatrix) = ld.readFiles (thefilename)#'../logs/4N2M')
 
 # Select an index nearest to a target
 ix = ld.selectIndex (x, y, (trgx,trgy))
@@ -33,30 +34,33 @@ ix = ld.selectIndex (x, y, (trgx,trgy))
 #print ('x range is {0} to {1}, y range is {2} to {3}'.format(min(x),max(x),min(y),max(y)))
 print ('ix is {0} for which coords are ({1},{2})'.format(ix, x[ix], y[ix]))
 
-# Do a plot
+# Do some plots
 #pt.trace (cmatrix, ix, t, 'c')
 #pt.trace (amatrix, ix, t, 'a')
 #pt.trace2 (nmatrix, ix, t, 'n')
-pt.trace3 (amatrix, cmatrix, nmatrix, ix, t, 'Comparative')
+#pt.trace3 (amatrix, cmatrix, nmatrix, ix, t, 'Comparative')
 
 # Plot the combined sum of n and c, which should remain constant
-F1 = plt.figure (figsize=(8,8))
-f1 = F1.add_subplot(1,1,1)
-# Plot sum of all connections made for all c
-csum = np.sum(np.sum(cmatrix[:,:,:],1), 0)
-f1.plot(t, csum)
-nsum = np.sum(nmatrix,0)
-f1.plot(t, nsum)
-f1.plot(t, csum+nsum)
-f1.legend (('sum c','sum n','sum (n+c)'))
-f1.set_title ('sums');
+plot_timedepstuff = 0
+if plot_timedepstuff:
+    F1 = plt.figure (figsize=(8,8))
+    f1 = F1.add_subplot(1,1,1)
+    # Plot sum of all connections made for all c
+    csum = np.sum(np.sum(cmatrix[:,:,:],1), 0)
+    f1.plot(t, csum)
+    nsum = np.sum(nmatrix,0)
+    f1.plot(t, nsum)
+    f1.plot(t, csum+nsum)
+    f1.legend (('sum c','sum n','sum (n+c)'))
+    f1.set_title ('sums');
 
+cnum = int(0)
+pt.surface (cmatrix[cnum,:,trgt], x, y, ix, 'c{0}'.format(cnum))
 cnum = int(1)
 pt.surface (cmatrix[cnum,:,trgt], x, y, ix, 'c{0}'.format(cnum))
-#pt.surface (amatrix[cnum,:,trgt], x, y, ix, 'a{0}'.format(cnum))
 pt.surface (nmatrix[:,trgt], x, y, ix, 'n')
-pt.surface2 (nmatrix[:,trgt], x, y, ix, 'n 2')
+#pt.surface2 (nmatrix[:,trgt], x, y, ix, 'n 2')
 
-pt.surfaces (cmatrix[:,:,trgt], amatrix[:,:,trgt], nmatrix[:,trgt], x, y, 'All variables')
+#pt.surfaces (cmatrix[:,:,trgt], amatrix[:,:,trgt], nmatrix[:,trgt], x, y, 'All variables')
 
 plt.show()

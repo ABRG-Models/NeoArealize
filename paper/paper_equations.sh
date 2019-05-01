6c48cd1844e9aedab50dc864ed3aed904df4f17a
Seb James <seb.james@sheffield.ac.uk>
2019-05-01 14:56:55 +0100

    Reduce the number of equations we show for the Karbowski system

--- paper/paper_equations.sh
+++ paper/paper_equations.sh
@@ -3,14 +3,10 @@
 # Use convert to snip out bits of the rendered latex into png images
 # suitable for Google docs
 
-convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x675+390+1255 "Eqns_01_to_03.jpg" &
+convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x655+390+1275 "Eqns_01_to_03.jpg" &
 
 convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+1995 "Eqns_04_to_04.jpg" &
 
-convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x675+390+2735 "Eqns_05_to_07.jpg" &
-
-convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+3465 "Eqns_08_to_08.jpg" &
-
-convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x235+390+3885 "Eqns_09_to_09.jpg" &
+convert -verbose -density 450 -trim "paper_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+2420 "Eqns_05_to_05.jpg" &
 
 wait

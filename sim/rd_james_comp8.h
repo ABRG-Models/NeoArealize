738bf625cbfc7809366bb882709335c15510d239
Seb James <seb.james@sheffield.ac.uk>
2019-06-05 14:29:08 +0100

    A couple of N=3 configs

--- sim/rd_james_comp8.h
+++ sim/rd_james_comp8.h
@@ -16,6 +16,11 @@ public:
     alignas(vector<Flt>) vector<Flt> sum_a;
 
     /*!
+     * An N element vector holding the initial sum of a_i for each TC type.
+     */
+    alignas(vector<Flt>) vector<Flt> sum_a_init;
+
+    /*!
      * Simple constructor; no arguments. Just calls base constructor.
      */
     RD_James_comp8 (void)
@@ -25,10 +30,20 @@ public:
     virtual void allocate (void) {
         RD_James_norm<Flt>::allocate();
         this->resize_vector_param (this->sum_a, this->N);
+        this->resize_vector_param (this->sum_a_init, this->N);
     }
 
     virtual void init (void) {
         RD_James_norm<Flt>::init();
+        // Now compute sum of a and record this as sum_a_init.
+        for (unsigned int i = 0; i < this->N; ++i) {
+            this->sum_a_computation(i);
+        }
+        this->sum_a_init = this->sum_a;
+        // Print these on screen
+        for (unsigned int ii = 0; ii < this->sum_a_init.size(); ++ii) {
+            cout << "sum_a_init["<<ii<<"] = " << this->sum_a_init[ii] << endl;
+        }
     }
 
     /*!
@@ -56,7 +71,9 @@ public:
      */
     virtual Flt transfer_a (const Flt& _a, const unsigned int _i) {
         // Divisive normalization step
-        Flt a_rtn = this->nhex * _a / this->sum_a[_i];
+        //Flt a_rtn = this->nhex * _a / this->sum_a[_i];
+        // Divisive norm with initial sum multiplier
+        Flt a_rtn = this->sum_a_init[_i] * _a / this->sum_a[_i];
         // Prevent a from becoming negative, necessary only when competition is implemented:
         return (a_rtn < 0.0) ? 0.0 : a_rtn;
     }

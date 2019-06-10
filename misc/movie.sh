a70cc0a629930671beb4ef0e0c9bed84412bb1e7
Seb James <seb.james@sheffield.ac.uk>
2019-06-10 23:29:52 +0100

    Change initial innervation in 5N1M_withcomp.json to be non-Gaussian and increase initial noise. Add a new 25N2M system

--- misc/movie.sh
+++ misc/movie.sh
@@ -6,5 +6,5 @@ fi
 
 for i in contours connections axonbranch a_contours; do
     touch ${i}.mp4
-    ffmpeg -r 60 -i ${i}_%05d.png -vb 5MB -vcodec mpeg4 ${i}.mp4 -y
+    ffmpeg -r 20 -i ${i}_%05d.png -vb 5MB -vcodec mpeg4 ${i}.mp4 -y
 done

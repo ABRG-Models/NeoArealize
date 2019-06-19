c7e0718c8c8d28f3b80d90dbdf9ad48583916903
Seb James <seb.james@sheffield.ac.uk>
2019-06-19 17:57:53 +0100

    More updates to equations

--- paper/barrel_equations.sh
+++ paper/barrel_equations.sh
@@ -13,9 +13,6 @@ convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -anti
 # With competition
 convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x700+390+3070 "WBEqns_06_to_08.jpg" &
 
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+4020 "WBEqns_09_to_09.jpg" &
-
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x225+390+4500 "WBEqns_10_to_10.jpg" &
-
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x225+390+4000 "WBEqns_09_to_09.jpg" &
 
 wait

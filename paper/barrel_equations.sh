d9cd269fe2021d19da1eaf979e274ea6cceecf0f
Seb James <seb.james@sheffield.ac.uk>
2019-06-19 13:59:52 +0100

    Updated equations, starting with unmodified Karb equations

--- paper/barrel_equations.sh
+++ paper/barrel_equations.sh
@@ -3,12 +3,19 @@
 # Use convert to snip out bits of the rendered latex into png images
 # suitable for Google docs
 
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x725+390+1275 "WBEqns_01_to_03.jpg" &
+# Basic Karbowski
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x700+390+1275 "WBEqns_01_to_03.jpg" &
 
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+2240 "WBEqns_04_to_04.jpg" &
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+2160 "WBEqns_04_to_04.jpg" &
 
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+2700 "WBEqns_05_to_05.jpg" &
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+2620 "WBEqns_05_to_05.jpg" &
 
-convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+3250 "WBEqns_06_to_06.jpg" &
+# With competition
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x700+390+3070 "WBEqns_06_to_08.jpg" &
+
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x245+390+4020 "WBEqns_09_to_09.jpg" &
+
+convert -verbose -density 450 -trim "barrel_equations.pdf[0]" -quality 100 -antialias -flatten -crop 2950x225+390+4500 "WBEqns_10_to_10.jpg" &
+
 
 wait
